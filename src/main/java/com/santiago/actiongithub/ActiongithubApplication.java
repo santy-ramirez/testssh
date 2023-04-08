cec8acb206e37459e403d5ec45c22638d473262e
santiago <santysjr956@gmail.com>
2023-04-07 21:57:24 -0300

    Added github action

---
+++ src/main/java/com/santiago/actiongithub/ActiongithubApplication.java
@@ -0,0 +1,20 @@
+package com.santiago.actiongithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ActiongithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiongithubApplication.class, args);
+	}
+	@GetMapping("/helloworld")
+	public String hello() {
+		return"Hello World! heroku";
+	}
+
+}
